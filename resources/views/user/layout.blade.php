7cd9a4fcbf35587fb3b5ef4125e82df2756d4e50
Hang Duong Khoi <khoihd.124010123099@vtc.edu.vn>
2024-12-13 00:40:03 +0700

    reponsive Home and Layout

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="overflow-x-hidden w-screen">
 
 <head>
     <meta charset="UTF-8">
@@ -7,9 +7,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Nulltifly')</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.dashjs.org/latest/dash.all.min.js"></script>
-    
+
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/playout.css'])
     @stack('styles')
     <style>
@@ -38,47 +40,49 @@
             display: flex;
         }
 
-        .search-form {
-            width: 60%;
-            position: relative;
-            display: flex;
-            justify-content: space-evenly;
-            align-items: center;
+        body.lock-scroll {
+            overflow: hidden;
+            touch-action: none;
         }
 
-        .search-form input {
-            width: 90%;
+        #search-form input[type="text"] {
+            height: 2.5rem;
+            width: 100%;
+            font-size: 1.5rem;
+            border-radius: 20px;
         }
 
-        .search-form input[type="text"] {
-            right: 0;
-        }
+        /* #sidebar {
+            z-index: 50;
+        } */
 
-        .search-song-icon i {
-            position: relative;
-            left: 0;
-            top: 0;
-            font-size: 20px;
-            color: #f5efef;
-            cursor: pointer;
+        #overlayphone {
+            z-index: 40;
         }
     </style>
 </head>
 
-<body>
+<body class="m-0 font-sans bg-[#202938] text-white overflow-x-hidden w-screen">
 
-    <div class="sidebar">
-        <a class="no-hover" href="{{ route('home') }}">
-            <img alt="Logo" height="100" src="http://localhost:8000/images/profile/logo-home.png"
-                width="100" />
+    <!-- Nút mở sidebar trên điện thoại -->
+    <button id="toggleSidebar" class="lg:hidden text-white fixed top-6 left-4 z-50 text-2xl">☰</button>
+    <div id="overlayphone" class="fixed inset-0 bg-black bg-opacity-50 hidden z-40"></div>
+    {{-- SideBar --}}
+    <div id="sidebar"
+        class="sidebar w-[250px] bg-[#1a1616] min-h-screen fixed pt-5 transform -translate-x-full lg:translate-x-0 transition-transform duration-300 ">
+        <a class="no-hover block text-white px-8 py-[11px] no-underline text-[18px] hover:bg-[#1a1616]"
+            href="{{ route('home') }}">
+            <img class="block mx-auto mb-5 w-36 cursor-pointer" alt="Logo" height="100"
+                src="http://localhost:8000/images/profile/logo-home.png" width="100" />
         </a>
-        <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }}">Home</a>
+        <a href="{{ route('home') }}"
+            class="{{ request()->routeIs('home') ? 'active' : '' }} block text-white px-8 py-[11px] no-underline text-[18px] hover:bg-gray-700">Home</a>
         <a href="{{ route('library') }}" id="librarys"
-            class="{{ request()->routeIs('library') ? 'active' : '' }}">Library</a>
+            class="{{ request()->routeIs('library') ? 'active' : '' }} block text-white px-8 py-[11px] no-underline text-[18px] hover:bg-gray-700">Library</a>
         <button id="createPlaylistBtn" class="btn-create-playist" onclick="NewPlaylist()"> + Create Playlist </button>
         @foreach ($playlists as $playlist)
             <a href="{{ route('playlist', $playlist->id) }}"
-                class="{{ request()->is('get-song-in-playlist/' . $playlist->id) ? 'active' : '' }}">{{ $playlist->name }}</a>
+                class="{{ request()->is('get-song-in-playlist/' . $playlist->id) ? 'active' : '' }} block text-white px-8 py-[11px] no-underline text-[18px] hover:bg-gray-700">{{ $playlist->name }}</a>
         @endforeach
     </div>
 
@@ -87,7 +91,8 @@
         <!-- Popup form tạo playlist -->
         <div id="createPlaylistPopup"
             class="hidden fixed inset-0 bg-gray-600 bg-opacity-80 flex items-center justify-center z-50">
-            <div class="bg-gray-800 w-full max-w-lg p-6 rounded-lg shadow-lg relative">
+            <div
+                class="bg-gray-800 w-full max-w-lg sm:max-w-md md:max-w-sm lg:max-w-lg xl:max-w-xl p-6 rounded-lg shadow-lg relative mx-4">
                 <!-- Nút đóng -->
                 <button id="closePopupBtn"
                     class="absolute top-4 right-4 text-gray-400 hover:text-gray-300 text-xl focus:outline-none">
@@ -95,7 +100,9 @@
                 </button>
 
                 <!-- Tiêu đề -->
-                <h2 class="text-2xl font-bold text-center text-white mb-6">Create Playlist</h2>
+                <h2 class="text-xl sm:text-2xl lg:text-3xl font-bold text-center text-white mb-6">
+                    Create Playlist
+                </h2>
 
                 <!-- Form -->
                 <form id="createPlaylistForm" method="POST" class="space-y-4">
@@ -116,15 +123,16 @@
         </div>
     </div>
 
-
-    <div class="main-content !pb-[100px]">
-        <div class="header p-1 flex justify-between items-center">
+    <div class="main-content !pb-[100px] ml-[250px] p-5">
+        <div class="header pb-3 flex justify-between items-center">
             <!-- Search -->
-            <form action="{{ route('searchsong') }}" method="get" class="search-form" id="search-form">
+            <form action="{{ route('searchsong') }}" method="get"
+                class="relative relative flex items-center justify-center w-full max-w-[15rem] lg:max-w-[45rem] px-4 md:px-0" id="search-form">
                 <input name="query" placeholder="Bạn đang tìm kiếm gì?" type="text" id="query"
-                    class="bg-black text-white rounded-full py-2 pl-10 pr-4 focus:outline-none" />
-                <button type="submit" class="search-song-icon">
-                    <i class="fa-solid fa-magnifying-glass fa-lg"></i>
+                    class="bg-black text-white rounded-full h-[2.5rem] w-full text-lg lg:text-xl pl-4 sm:pl-10 p-3 focus:outline-none placeholder:text-sm lg:placeholder:text-xl placeholder:translate-y-[-5px] lg:placeholder:translate-y-[-3px]" />
+                <button type="submit"
+                    class="absolute right-4 flex items-center justify-center w-10 h-10 md:w-12 md:h-12 bg-transparent border-none cursor-pointer">
+                    <i class="fa-solid fa-magnifying-glass text-[16px] md:text-[20px] text-[#f5efef]"></i>
                 </button>
             </form>
 
@@ -157,11 +165,10 @@
                 </div>
             @else
                 {{-- Nếu người dùng chưa đăng nhập --}}
-                <div class="auth-links">
-                    <a onclick="showLoginForm()" class="login-link">Login</a>
-                    <span class="separator">/</span>
-                    <a onclick="showRegisterForm()" class="register-link">Register</a>
+                <div class="auth-links flex justify-between items-center mb-3">
+                    <a onclick="showLoginForm()" class="text-[#e3e3ea] text-base no-underline hover:text-[#68aee0]">Login</a>
                 </div>
+                
             @endif
         </div>
         @yield('content')
@@ -220,6 +227,64 @@
         class="hidden fixed top-4 right-4 bg-blue-500 text-white py-2 px-4 rounded-lg shadow-lg z-50"></div>
     @include('components.footer')
     <script>
+        //*Phần liên quan tới reponsive*
+
+        //Nút Hiện sidebar trên điện thoại
+        const toggleSidebar = document.getElementById('toggleSidebar');
+        const sidebar = document.getElementById('sidebar');
+        const overlay = document.getElementById('overlayphone');
+        const maincontent = document.querySelector('.main-content');
+        const body = document.body;
+
+        // Hàm mở/đóng sidebar
+        const openSidebar = () => {
+            sidebar.classList.remove('-translate-x-full');
+            overlay.classList.remove('hidden');
+            body.classList.add('lock-scroll'); // Thêm class chặn cuộn
+        };
+        const closeSidebar = () => {
+            sidebar.classList.add('-translate-x-full');
+            overlay.classList.add('hidden');
+            body.classList.remove('lock-scroll'); // Xóa class chặn cuộn
+        };
+
+        // Xử lý nút mở sidebar
+        toggleSidebar.addEventListener('click', () => {
+            if (sidebar.classList.contains('-translate-x-full')) {
+                openSidebar();
+            } else {
+                closeSidebar();
+            }
+        });
+        // Xử lý khi nhấn vào overlay
+        overlay.addEventListener('click', closeSidebar);
+
+        // Hàm tùy chỉnh khi thay đổi kích thước màn hình
+        function handleResize() {
+            if (window.innerWidth >= 1024) { // Kích thước màn hình lớn (desktop)
+                sidebar.style.zIndex = ''; // Xóa z-index của sidebar để không bị đè footer
+                maincontent.style.marginLeft = '250px'; // Đặt margin-left cho main content
+            } else { // Kích thước màn hình nhỏ (điện thoại, tablet)
+                sidebar.style.zIndex = '50'; // Đặt z-index cho sidebar
+                maincontent.style.marginLeft = '0'; // Xóa margin-left
+            }
+        }
+
+        // Gọi hàm ngay khi tải trang
+        handleResize();
+
+        // Lắng nghe sự kiện thay đổi kích thước cửa sổ
+        window.addEventListener('resize', handleResize);
+
+
+
+
+
+
+
+
+
+
         document.getElementById('search-form').addEventListener('submit', function(event) {
             event.preventDefault();
             const searchInput = document.getElementById('query');
@@ -244,16 +309,16 @@
                     .catch(error => {
                         console.error('Error:', error);
                     });
-    
+
                 // Tiến hành tìm kiếm như bình thường
                 window.location.href = '/search?query=' + encodeURIComponent(query);
             }
         });
-    
+
         function flash(message, type = 'success') {
             // Lấy phần tử flash message
             const flashMessage = document.getElementById('flash-message');
-    
+
             // Cập nhật nội dung và kiểu dáng dựa trên loại thông báo
             flashMessage.textContent = message;
             flashMessage.className =
@@ -275,22 +340,22 @@
 
             const avatar = document.querySelector('#avatar'); // Lấy phần tử #avatar
             const avatarPopup = document.querySelector('.avatar-popup'); // Lấy phần tử pop-up
-    
+
             if (!avatar || !avatarPopup) return;
-    
+
             // Mở popup khi nhấn vào avatar
             avatar.addEventListener('click', function(e) {
                 e.stopPropagation(); // Ngăn chặn sự kiện ngoài từ việc ẩn pop-up
                 avatarPopup.classList.toggle('block'); // Thêm hoặc xóa class 'block' cho popup
             });
-    
+
             // Ẩn popup khi click ra ngoài
             document.addEventListener('click', function(e) {
                 if (!avatarPopup.contains(e.target) && !avatar.contains(e.target)) {
                     avatarPopup.classList.remove('block'); // Ẩn pop-up
                 };
             });
-    
+
             // Ngăn pop-up bị tắt khi click bên trong
             avatarPopup.addEventListener('click', function(e) {
                 e.stopPropagation();
@@ -298,28 +363,28 @@
             // Lấy các phần tử popup và nút
             var playlistPopup = document.getElementById("createPlaylistPopup");
             var closePopupBtn = document.getElementById("closePopupBtn");
-    
+
             // Đóng popup khi nhấn nút đóng
             closePopupBtn.addEventListener("click", function() {
                 playlistPopup.style.display = "none";
             });
-    
+
             // Đóng popup nếu người dùng nhấn ra ngoài popup
             window.addEventListener("click", function(event) {
                 if (event.target == playlistPopup) {
                     playlistPopup.style.display = "none";
                 }
             });
-    
+
             const toggleLyricsBtn = document.getElementById("toggleLyricsIcon"); // Nút play làm trigger
             const lyricPopup = document.getElementById("lyricPopup");
-    
+
             let isLyricsVisible = false; // Trạng thái hiển thị lyrics
-    
+
             // Toggle popup lyrics
             toggleLyricsBtn.addEventListener("click", () => {
                 isLyricsVisible = !isLyricsVisible;
-    
+
                 if (isLyricsVisible) {
                     // Mở popup, thêm độ trễ nhỏ để hiệu ứng trượt lên
                     lyricPopup.classList.remove("hidden");
@@ -341,17 +406,17 @@
                 }
             });
         });
-    
+
         // Open popup
         function openPopup() {
             document.getElementById('overlay').classList.add('active');
         }
-    
+
         // Close popup
         function closePopup() {
             document.getElementById('overlay').classList.remove('active');
         }
-    
+
         // Function to create a new playlist
         function NewPlaylist() {
             const isLoggedIn = {{ Auth::check() ? 'true' : 'false' }};
@@ -373,7 +438,7 @@
                 flash('Vui lòng đăng nhập để tạo playlist!', 'error');
             }
         }
-    
+
         function createNewPlaylist() {
             console.log('create new playlist');
             let name = document.getElementById('title-playlist').value.trim();
@@ -382,7 +447,7 @@
                 alert('Vui lòng nhập đầy đủ thông tin.');
                 return;
             }
-    
+
             $.ajax({
                 url: '/create-playlist',
                 method: 'POST',
@@ -399,7 +464,7 @@
                     popup.style.display = "none";
                     document.getElementById('title-playlist').value = '';
                     flash('Danh sách phát mới đã được thêm!', 'success');
-    
+
                 },
                 error: function(xhr) {
                     console.error('Error:', xhr.responseText);
@@ -407,8 +472,8 @@
                 }
             });
         }
-    
-    
+
+
         function toggleSelection(element) {
             const checkIcon = element.querySelector('i.fas.fa-check-circle');
             const playlistId = element.getAttribute('data-playlist-id');
@@ -425,7 +490,7 @@
                 checkIcon.classList.toggle('!hidden');
             }
         }
-    
+
         function confirmSelection() {
             const songId = document.getElementById('footer').getAttribute('data-song-id');
             selectedPlaylists.forEach(playlistId => {
@@ -465,7 +530,7 @@
                     })
                 });
             });
-    
+
             // Sau khi gửi, đóng popup
             closePopup();
         }
